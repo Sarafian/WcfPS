4bd02957361d4345240f771d82ad2a52f981d1be
Alex Sarafian <asarafian@sdl.com>
2016-06-02 15:51:48 +0200

    Small fixed for publishing

--- Publish/PublishToGallery.ps1
+++ Publish/PublishToGallery.ps1
@@ -6,13 +6,7 @@ Param (
     [string]$NuGetApiKey
 )
 
-& "$PSScriptRoot\..\Pester\Test-All.ps1"
-if($LASTEXITCODE -ne 0)
-{
-    Write-Error "Stopping"
-    exit -1
-}
 & "$PSScriptRoot\PreparePSD1.ps1"
 
-#Publish-Module -Path "$PSScriptRoot\..\Modules\MarkdownPS" -NuGetApiKey $NuGetApiKey 
+Publish-Module -Path "$PSScriptRoot\..\Modules\WcfPS" -NuGetApiKey $NuGetApiKey 
 
